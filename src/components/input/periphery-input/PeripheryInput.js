dbc63ef7ccaa636f2a2f71d5cf3a59cc5bd3e8e3
1337Herman228 <geragets@gmail.com>
2024-12-14 02:08:39 +0300

    minor fixes

--- src/components/input/periphery-input/PeripheryInput.js
+++ src/components/input/periphery-input/PeripheryInput.js
@@ -1,82 +1,74 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import AuthInput from '../auth-input/AuthInput';
-import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
-import './PeripheryInput.scss'
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import AuthInput from "../auth-input/AuthInput";
+import { CloseOutlined, PlusOutlined } from "@ant-design/icons";
+import "./PeripheryInput.scss";
 
 const startValue = [
     {
         id: uuidv4(),
-        key: '',
-        value: '',
+        key: "",
+        value: "",
     },
-]
+];
 
-const PeripheryInput = (
-    {
-        register, 
-        unregister,
-        errors, 
-        defaultValue = null,
-    }) => {
-
+const PeripheryInput = ({
+    register,
+    unregister,
+    errors,
+    defaultValue = null,
+}) => {
     const [value, setValue] = useState(defaultValue || startValue);
 
-    const add = () =>{
-       setValue([...value, {id: uuidv4(), key: '', value: ''} ])
-    }
+    const add = () => {
+        setValue([...value, { id: uuidv4(), key: "", value: "" }]);
+    };
 
-    const remove = (id) =>{
-        const filteredValue = value.filter(item => item.id !== id);
+    const remove = (id) => {
+        const filteredValue = value.filter((item) => item.id !== id);
         setValue(filteredValue);
-        unregister('key/' + id);
-        unregister('value/' + id);
-    }
-
-    // const edit = ({id, newKey, newValue}) =>{
-    //    const editItem =  value.find(item => item.id === id)
-    //    editItem.key = newKey
-    //    editItem.value = newValue
-    // }
-
-    // console.log(value)
+        unregister("key/" + id);
+        unregister("value/" + id);
+    };
 
     return (
-        <div className='periphery-input'>
+        <div className="periphery-input">
             {value.map((item) => (
-                <div className='input-item'>
-                    <AuthInput 
+                <div className="input-item">
+                    <AuthInput
                         defaultValue={item.key}
-                        labelText='Характеристика' 
+                        labelText="Характеристика"
                         name={`key/${item.id}`}
-                        minLength={0} 
+                        minLength={0}
                         require={true}
-                        register={register} 
+                        register={register}
                         errors={errors}
                     />
-                    <AuthInput 
+                    <AuthInput
                         defaultValue={item.value}
-                        labelText='Значение' 
+                        labelText="Значение"
                         name={`value/${item.id}`}
-                        minLength={0} 
+                        minLength={0}
                         require={true}
-                        register={register} 
+                        register={register}
                         errors={errors}
                     />
-                    <CloseOutlined className='input-item__remove' onClick={() => remove(item.id)} />
+                    <CloseOutlined
+                        className="input-item__remove"
+                        onClick={() => remove(item.id)}
+                    />
                 </div>
             ))}
-            <button 
-                onClick={add} 
-                type='button'
-                className='input-item__add-btn main-color-submit-btn'
+            <button
+                onClick={add}
+                type="button"
+                className="input-item__add-btn main-color-submit-btn"
             >
                 <PlusOutlined />
-                <span className='input-item__add-btn-text'>Добавить поле</span>
+                <span className="input-item__add-btn-text">Добавить поле</span>
             </button>
-
         </div>
-    )
-}
+    );
+};
 
 export default PeripheryInput;
